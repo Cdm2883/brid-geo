7abc66934e2772e62be826fe3217b17fffa55376
Cdm2883 <Cdm2883@hotmail.com>
2024-07-19 20:37:44 +0800

    refactor: some methods in PluginRegisterRelay cancel changes to "this"

--- src/bridgeo/plugin/register/plugin-register-relay.ts
+++ src/bridgeo/plugin/register/plugin-register-relay.ts
@@ -1,17 +1,18 @@
 import PluginRegister from "@/bridgeo/plugin/register/plugin-register";
 import { PacketOptionsStub, PacketsReceiver, PacketStub } from "@/bridgeo/relay/packet-bus";
 import CommonRelay from "@/bridgeo/relay/relay";
-import { RelayContext, RelayCreatingMaterial, relays } from "@/bridgeo/relay/starter";
+import { BridgeoRelayOptions, RelayContext, RelayCreatingMaterial, relays } from "@/bridgeo/relay/starter";
 import { binding } from "@/bridgeo/utils/js/functions";
+import { ArrayShift } from "@/bridgeo/utils/js/type-utils";
 
-export interface IPluginRegisterRelay extends PacketsReceiver {
+export interface IPluginRegisterRelay {
     onRelayCreating?(material: RelayCreatingMaterial): void;
     onRelayCreated?(relay: CommonRelay, material: Readonly<RelayCreatingMaterial>): void;
     onRelayJoined?(context: RelayContext): void;
-    onClientBound?<T>(this: RelayContext, packet: PacketStub<T>, options: PacketOptionsStub<T>): void;
-    onServerBound?<T>(this: RelayContext, packet: PacketStub<T>, options: PacketOptionsStub<T>): void;
-    onUpstream?(this: RelayContext, buffer: Buffer, options: { canceled: boolean }): void;
-    onDownstream?(this: RelayContext, buffer: Buffer, options: { canceled: boolean }): void;
+    onClientBound?<T>(context: RelayContext, packet: PacketStub<T>, options: PacketOptionsStub<T>): void;
+    onServerBound?<T>(context: RelayContext, packet: PacketStub<T>, options: PacketOptionsStub<T>): void;
+    onUpstream?(context: RelayContext, buffer: Buffer, options: { canceled: boolean }): void;
+    onDownstream?(context: RelayContext, buffer: Buffer, options: { canceled: boolean }): void;
 }
 export class PluginRegisterRelay extends PluginRegister<IPluginRegisterRelay> {
     register(handler: IPluginRegisterRelay) {
@@ -22,7 +23,7 @@ export class PluginRegisterRelay extends PluginRegister<IPluginRegisterRelay> {
         if (onRelayCreated) {
             relays.forEach(relay => onRelayCreated(relay, {
                 class: relay.constructor as typeof CommonRelay,
-                options: relay.options,
+                options: relay.options as BridgeoRelayOptions,
                 logger: relay.logger
             }));
             this.plugin.lifecycle.on('relay.created', onRelayCreated);
@@ -31,26 +32,26 @@ export class PluginRegisterRelay extends PluginRegister<IPluginRegisterRelay> {
         const handlePackets = (context: RelayContext) => {
             handler.onRelayJoined?.(context);
 
-            const onClientBound = handler.onClientBound?.bind(context);
-            const onServerBound = handler.onServerBound?.bind(context);
+            const onClientBound = handler.onClientBound;
+            const onServerBound = handler.onServerBound;
             if (onClientBound || onServerBound) {
                 const receiver: PacketsReceiver = {
-                    onClientBound,
-                    onServerBound,
+                    onClientBound: (...args) => onClientBound?.(context, ...args),
+                    onServerBound: (...args) => onServerBound?.(context, ...args),
                 };
                 context.packets.register(receiver);
                 this.plugin.lifecycle.on('self.unload', () => context.packets.unregister(receiver));
             }
 
-            const onUpstream = handler.onUpstream?.bind(context);
+            const onUpstream = handler.onUpstream;
             if (onUpstream) {
-                const args = [ 'upstream', onUpstream ] as const;
+                const args = [ 'upstream', (...args: ArrayShift<Parameters<typeof onUpstream>>) => onUpstream(context, ...args) ] as const;
                 context.client.on(...args);
                 this.plugin.lifecycle.on('self.unload', () => context.client.off(...args));
             }
             const onDownstream = handler.onDownstream?.bind(context);
             if (onDownstream) {
-                const args = [ 'downstream', onDownstream ] as const;
+                const args = [ 'downstream', (...args: ArrayShift<Parameters<typeof onDownstream>>) => onDownstream(context, ...args) ] as const;
                 context.client.on(...args);
                 this.plugin.lifecycle.on('self.unload', () => context.client.off(...args));
             }
--- src/bridgeo/utils/js/type-utils.ts
+++ src/bridgeo/utils/js/type-utils.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export type Enumerate<T extends number, R extends number[] = []> = R['length'] extends T
     ? R[number]
     : Enumerate<T, [R['length'], ...R]>
 
 export type RangeInteger<Min extends number, Max extends number> = Exclude<Enumerate<Max>, Enumerate<Min>>
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ArrayIndexes<T extends any[]> = T extends { length: infer L extends number } ? Enumerate<L> : never;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ArrayStringifyIndexes<T extends any[]> = Exclude<keyof T, keyof any[]>;
+
+export type ArrayShift<T extends any[]> = T extends [unknown, ...infer Rest] ? Rest : never;
